f8e637f6cb3c86d5c8df737aacf1478ad8bc7d92
red-ait <redouanmkdem@gmail.com>
2020-02-11 12:42:41 +0100

    e

--- src/main/java/ProbOne.java
+++ src/main/java/ProbOne.java
@@ -1,12 +1,18 @@
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class ProbOne {
 
     public static void main(String[] args) {
-        int[][] objs= {{22,10},{20,5},{20,5}};
 
-        valeurMax(objs, 10);
+        // 0 => valeur
+        // 1 => poid
+        int[][] objs= {{22,10},{20,5},{20,5}};
+        System.out.println(valeurMax(objs,10));
 
     }
 
@@ -14,68 +20,72 @@ public class ProbOne {
         // 0 => valeur
         // 1 => poid
         int valuerMax = 0;
-        int poid = 0;
+        List<Integer> range = IntStream.rangeClosed(0, objets.length - 1)
+                .boxed().collect(Collectors.toList());
+        List<List<Integer>> indexes = new ArrayList<>();
+        for(int i = 1 ; i <= objets.length ; i++)
+            indexes.addAll(combine(range,range, i));
 
+        for(List<Integer> idxs : indexes) {
+            int valeur = 0;
+            int poid = 0;
+            for(int i : idxs) {
+                valeur += objets[i][0];
+                poid += objets[i][1];
+            }
+            if(poid <= M && valeur > valuerMax)
+                valuerMax = valeur;
+        }
+
+        return  valuerMax;
+    }
+
+    public static List<List<Integer>> combine(List<Integer> list,List<Integer> lastList, int Longeur) {
+        List<List<Integer>> resultat = new ArrayList<>();
+
+        for (Integer s : list) {
+            if (Longeur == 1) {
+                List<Integer> l = new ArrayList<Integer>();
+                l.add(s);
+                resultat.add(l);
+            } else {
+                for(List<Integer> last : combine(list, lastList, Longeur - 1)) {
+                    boolean flag = false;
+                    List<Integer> l = new ArrayList<>();
+                    l.addAll(last);
+                    l.add(s);
+                    for(List<Integer> res : resultat) {
+                        Collections.sort(res);
+                        Collections.sort(l);
+                        if(res.equals(l)) {
+                            flag = true;
+                        }
+                    }
+                    if (!flag)
+                        resultat.add(l);
+                }
+           }
+        }
         boolean flag = false;
         do {
             flag = false;
-            for(int i = 0; i < objets.length - 1; i ++)  {
-                if(objets[i][0] < objets[i + 1][0]){
-                    int[] aux = objets[i];
-                    objets[i] = objets[i + 1];
-                    objets[i + 1] = aux;
-                    flag = true;
-                }
+            int k = 0;
+            for( k = 0; k < resultat.size() && !flag ; k++) {
+                for(int i = 0 ; i< resultat.get(k).size() && !flag ; i++)
+                    for(int j = i + 1 ; j< resultat.get(k).size() && !flag ; j++) {
+                        if(resultat.get(k).get(i).equals(resultat.get(k).get(j))) {
+                            flag = true;
+                        }
+                    }
             }
+            if(flag)
+            resultat.remove(k - 1);
+
+
         } while (flag);
 
-        for(int i = 0; i < objets.length ; i ++)  {
-            if((objets[i][1] + poid) <= M) {
-                poid += objets[i][1];
-                valuerMax += objets[i][0];
-            }
-        }
-        return  valuerMax;
+        return resultat;
     }
-/*    public static List<List<Obj>> combinations(List<Obj> motList, int Longeur) {
-        List<List<Obj>> Resultat = new ArrayList<>();
 
-      for (int i = 0; i < motList.size() ; i++) {
-            System.out.println(i + 1    + "< >" + motList.size());
-            if (Longeur == 1) {
-                List<Obj> list = new ArrayList<>();
-                list.add(motList.get(i));
-                Resultat.add(list);
-            }
-            else {
-                if(i + 1 <= motList.size() - (i +1 ))  {
-                    List<Obj> ListIntermediaire = motList.subList(i + 1, motList.size() - (i +1 ));
-                    List<List<Obj>> CombiList = combinations(ListIntermediaire, Longeur - 1);
-                    for (List<Obj> s : CombiList) {
-                        List<Obj> list = s;
-                        list.add(motList.get(i));
-                        Resultat.add(list);
-                    }
-                } else {
-                    List<Obj> ListIntermediaire = motList.subList(motList.size() - (i +1 ),i + 1);
-                    List<List<Obj>> CombiList = combinations(ListIntermediaire, Longeur - 1);
-                    for (List<Obj> s : CombiList) {
-                        List<Obj> list = s;
-                        list.add(motList.get(i));
-                        Resultat.add(list);
-                    }
-                }
-            }
-        }
-        return Resultat;
-    }
-    private class Obj {
-        public int poid;
-        public int valeur;
 
-        public Obj(int poid, int valeur) {
-            this.poid = poid;
-            this.valeur = valeur;
-        }
-    }
-*/}
+}
